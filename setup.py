e570c3e6d9966ba4301dbb3672744be32658c74d
praveen.p <praveen.dsmlai@gmail.com>
2026-03-10 03:16:25 +0530

    My second Commit

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import find_packages,setup
 from typing import List 
 
+
+HYPEN_E_DOT='-e .'
 def get_requirements(filepath:str)->List[str]:
-    pass
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(filepath) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
     name = "Praveen",
